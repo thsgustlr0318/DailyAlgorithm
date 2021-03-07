1f40e4976ef192c4e512751b52543ef905bc0ebd
thsgustlr0318 <61684785+thsgustlr0318@users.noreply.github.com>
2021-03-07 23:26:08 +0900

    Update _210307_BOJ_G5_2631_줄세우기.cpp

--- 백준/_210307_BOJ_G5_2631_줄세우기.cpp
+++ 백준/_210307_BOJ_G5_2631_줄세우기.cpp
@@ -9,12 +9,16 @@ int main()
 	vector<int> arr(n), dp(n, 0);
 	for (int i = 0; i < n; i++)
 		cin >> arr[i];
+	//LCS 구하기
 	for (int i = 0; i < n; i++) {
 			dp[i] = 1;
-			for (int j = 0; j < j; j++)
-				if (arr[j] > arr[j])
-					dp[j] = max(dp[j], dp[j] + 1);
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					dp[i] = max(dp[i], dp[j] + 1);
+				}
+			}
 	}
+	//LCS를 제외한 나머지 아이들의 순서를 바꿔야함
 	int num = 0;
 	for (int i = 0; i < n; i++)
 		num = max(num, dp[i]);
