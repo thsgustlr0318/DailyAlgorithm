c9bf3d4e678f8b0bf25cbaed2c1780c451092538
thsgustlr0318 <61684785+thsgustlr0318@users.noreply.github.com>
2021-03-07 23:26:04 +0900

    Update _210307_BOJ_G5_1915_가장 큰 정사각형.cpp

--- 백준/_210307_BOJ_G5_1915_가장 큰 정사각형.cpp
+++ 백준/_210307_BOJ_G5_1915_가장 큰 정사각형.cpp
@@ -20,8 +20,9 @@ int main()
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
 			if (arr[i][j] == 0) continue;
+			//왼쪽 위쪽 모두 1인경우, dp[i][j] = dp[왼쪽 위쪽 대각선] 중 최소 + 1
 			if (arr[i - 1][j] == 1 && arr[i][j - 1] == 1)
-				dp[i][j] = min(dp[i][j-1], min(dp[i-1][j], dp[i - 1][j - 1])) + 1;
+				dp[i][j] = min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
 			else {
 				dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
 			}
@@ -30,5 +31,5 @@ int main()
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
 			res = max(res, dp[i][j]);
-	cout << res*res;
+	cout << res * res;
 }
