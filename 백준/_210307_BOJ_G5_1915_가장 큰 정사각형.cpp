0de84b71293bbab8eff6fb15ed652e551e6b77dd
thsgustlr0318 <61684785+thsgustlr0318@users.noreply.github.com>
2021-03-07 23:19:09 +0900

    Create _210307_BOJ_G5_1915_가장 큰 정사각형.cpp

---
+++ 백준/_210307_BOJ_G5_1915_가장 큰 정사각형.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+int dp[1001][1001], arr[1001][1001];
+int main()
+{
+	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+	int n, m, res = 0;
+	cin >> n >> m;
+
+	string str;
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+		for (int j = 0; j <= m; j++)
+			arr[i + 1][j + 1] = str[j] - '0';
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (arr[i][j] == 0) continue;
+			if (arr[i - 1][j] == 1 && arr[i][j - 1] == 1)
+				dp[i][j] = min(dp[i][j-1], min(dp[i-1][j], dp[i - 1][j - 1])) + 1;
+			else {
+				dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			res = max(res, dp[i][j]);
+	cout << res*res;
+}
